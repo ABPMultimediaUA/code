030dccfc79f0148173b9da58d3fa7d61fc43db65
Ivan <ivpm94@gmail.com>
2016-11-24 16:38:45 +0100

    Prototipo Estados Unity
    Máquina de estados simple implementada en Unity. Sin optimizar.

---
+++ DESARROLLO/HITO1/ITERACION 3/MaquinaEstados/Prototipo Unity/1414/Assets/Estados/Enemigo.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemigo : MonoBehaviour {
+
+
+	Estado state;
+	public GameObject g;
+	// Use this for initialization
+	void Start () {
+		g = this.gameObject;
+		state= g.GetComponent<Estado1> ();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log (state);
+		state.Update ();
+
+		if(Input.GetKeyDown(KeyCode.K)){
+		 Change();
+		}
+	}
+
+			void Change (){
+		if (state.id == 1) {
+			
+			state.enabled = false;
+			//state.Salida();
+			state=g.GetComponent<Estado2>();
+			state.enabled = true;
+			//state.Entrada
+
+		} else {
+			
+			state.enabled = false;
+			//state.Salida();
+			state=g.GetComponent<Estado1>();
+			state.enabled = true;
+			//state.Entrada
+		}
+		//Debug.Log (state);
+	}
+}
---
+++ DESARROLLO/HITO1/ITERACION 3/MaquinaEstados/Prototipo Unity/1414/Assets/Estados/Estado1.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Estado1 : Estado {
+
+
+
+
+	// Use this for initialization
+	public void Start () {
+		id = 1;
+		punto = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+
+
+			punto.transform.position = new Vector3 (punto.transform.position.x + 1, punto.transform.position.y, punto.transform.position.z);
+
+			
+
+
+	}
+}
