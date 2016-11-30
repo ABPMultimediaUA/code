5e66edaee9fa9718c0b0239a84f08eb678dae52e
DavidBrando <davidtbs@hotmail.es>
2016-11-30 15:49:11 +0100

    Cambios minimos
    No se avanza nada con el JSON

--- DESARROLLO/HITO1/ITERACION 3/V2/ejemplo/readJson.cpp
+++ DESARROLLO/HITO1/ITERACION 3/V2/ejemplo/readJson.cpp
@@ -14,20 +14,38 @@
 #include "readJson.h"
 #include "../rapidjson/filereadstream.h"
 #include "../rapidjson/document.h"
+#include "../rapidjson/pointer.h"
+#include "../rapidjson/istreamwrapper.h"
+#include <fstream>
 #include <cstdio>
 
 using namespace rapidjson;
 
+
 readJson::readJson() {
-    FILE* fp = fopen("../escena.json", "rb"); // non-Windows use "r"
+   /* FILE* fp = fopen("../escena.json", "rb"); // non-Windows use "r"
     char readBuffer[65536];
-    FileReadStream is(fp, readBuffer, sizeof(readBuffer));
+    FileReadStream is(fp, readBuffer, sizeof(readBuffer));*/
+    std::ifstream ifs("../escena.json");
+    IStreamWrapper is(ifs);
     Document d;
     d.ParseStream(is);
-    fclose(fp);
+   // fclose(fp);
     assert(d.HasMember("name"));
     assert(d["name"].IsString());
     printf("------------------------------->Primera Etiqueta = %s\n", d["name"].GetString());
+    
+    //printf("------------------------------->Primera Etiqueta = %s\n", d.MemberBegin()->value.GetString());
+
+
+    for (Value::ConstMemberIterator itr = d.MemberBegin(); itr != d.MemberEnd(); ++itr)
+    {
+        
+        //printf("Type of member %s is %s\n", itr->name.IsString(), itr->value.IsString());
+        printf("CACA\n");
+    }
+
+
 }
 
 readJson::readJson(const readJson& orig) {
