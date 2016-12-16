2ff1bd89320365c6e9923077418ad707e0370434
Ivan <ivpm94@gmail.com>
2016-12-16 16:49:09 +0100

    Base
    Preparación de estados como base para desarrollar el behavior tree.

---
+++ DESARROLLO/HITO1/ITERACION 4/estados/Nodo.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Nodo.cpp
+ * Author: RicharRisa
+ * 
+ * Created on 16 de diciembre de 2016, 16:28
+ */
+
+#include "Nodo.h"
+
+Nodo::Nodo() {
+}
+
+Nodo::Nodo(const Nodo& orig) {
+}
+
+Nodo::~Nodo() {
+}
+
---
+++ DESARROLLO/HITO1/ITERACION 4/estados/Nodo.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Nodo.h
+ * Author: RicharRisa
+ *
+ * Created on 16 de diciembre de 2016, 16:28
+ */
+
+#ifndef NODO_H
+#define NODO_H
+
+class Nodo {
+public:
+    Nodo();
+    Nodo(const Nodo& orig);
+    virtual ~Nodo();
+private:
+
+};
+
+#endif /* NODO_H */
+
