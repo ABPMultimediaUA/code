ec651ce86d1b02595392b8e20004658b457bd161
DavidBrando <davidtbs@hotmail.es>
2017-02-06 17:12:48 +0100

    Creacion navmeshes

---
+++ DESARROLLO/Juego/Startship 1414/Enemigos/navmeshes.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   navmeshes.cpp
+ * Author: User
+ * 
+ * Created on 6 de febrero de 2017, 17:11
+ */
+
+#include "navmeshes.h"
+
+navmeshes::navmeshes() {
+}
+
+navmeshes::navmeshes(const navmeshes& orig) {
+}
+
+navmeshes::~navmeshes() {
+}
+
---
+++ DESARROLLO/Juego/Startship 1414/Enemigos/navmeshes.h
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   navmeshes.h
+ * Author: User
+ *
+ * Created on 6 de febrero de 2017, 17:11
+ */
+
+#ifndef NAVMESHES_H
+#define NAVMESHES_H
+
+#include <irrlicht.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <math.h>
+using namespace irr;
+
+using namespace core;
+using namespace scene;
+using namespace video;
+using namespace io;
+using namespace gui;
+
+class navmeshes {
+public:
+    navmeshes();
+    navmeshes(const navmeshes& orig);
+    virtual ~navmeshes();
+private:
+
+    
+    
+};
+
+#endif /* NAVMESHES_H */
+
